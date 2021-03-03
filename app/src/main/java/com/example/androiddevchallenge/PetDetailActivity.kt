17b5f83c653b01451a5bff50c467ba8af4f9150e
zhaopf <2722246227@qq.com>
2021-03-03 17:14:36 +0800

    更新

--- app/src/main/java/com/example/androiddevchallenge/PetDetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/PetDetailActivity.kt
@@ -15,12 +15,17 @@
  */
 package com.example.androiddevchallenge
 
-import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -29,6 +34,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+
 
 class PetDetailActivity : AppCompatActivity() {
 
